63abebaa5055b94ef4cb96d9cb8057dfae866ccc
Sagos <sagoswins@gmail.com>
2021-08-23 19:08:00 -0300

    Melhoria no bot

--- AutoFish.py
+++ AutoFish.py
@@ -2,27 +2,29 @@
     05/08/2020 - A meta é criar um bot que coleta todos os tipos de algodão
     12/08/2020 - Não deu muito certo, vou tentar pescar peixe automaticamente
     14/08/2020 - Criando um bot de pesca
+    23/08/2021 - Voltei pra fazer melhorias
 '''
 
 
+import pyscreenshot as ImageGrab
 from time import sleep
 import pyautogui
 
 
-path = 'img/'  
+path = 'img\\'  
 
 
-def fishing():
+def start_fishing():
     try:
         x, y = pyautogui.locateCenterOnScreen(path+'skill.png', confidence=0.9)
     except:
         x, y = None,None
 
     if x and y:
-        print('Usando skill de pesca')
-        pyautogui.press('q')
+        pyautogui.press('q') # Trocar para usar a skill com o mouse
+        print('Utilizou habilidade: Escudo de água')
 
-    print('Iniciando nova pesca...')
+    print('Iniciando pesca')
     pyautogui.mouseUp()
     sleep(0.5)
     pyautogui.mouseDown()
@@ -30,15 +32,29 @@ def fishing():
     pyautogui.mouseUp()
     sleep(2)
 
-    catch_fish()
+    watch_water()
 
 
-def catch_fish():
+def capture_hook():
+    mouse = pyautogui.position()
+
+    hook = ImageGrab.grab(bbox=(mouse.x-15, mouse.y-15, mouse.x+15, mouse.y+15))
+
+    hook.save('hook.png')
+
+    return hook
+
+
+def watch_water():
+    hook = capture_hook()
+
     while True:
         try:
-            x, y = pyautogui.locateCenterOnScreen(path+'hook2.png', confidence=0.8)
+            x, y = pyautogui.locateCenterOnScreen(hook, confidence=0.8)
+            print('Encontrou')
         except:
             x, y = None,None
+            print('Não encontrou')
 
         if not x and not y:
             print('Fisgou um peixe')
@@ -46,10 +62,10 @@ def catch_fish():
             sleep(0.3)
             break
 
-    progressBar()
+    catch_fish()
 
 
-def progressBar():
+def catch_fish():
     while True:
         try:
             xRight, yRight = pyautogui.locateCenterOnScreen(path+'progressBarRight.png', confidence=0.92, region=(551,364,820,442))
@@ -74,8 +90,10 @@ def progressBar():
             pyautogui.mouseDown() # Pressiona o botão esquerdo
             
 
-    fishing()
+    start_fishing()
 
 
-sleep(5)
-fishing()
+
+sleep(7)
+start_fishing()
+# capture_hook()
